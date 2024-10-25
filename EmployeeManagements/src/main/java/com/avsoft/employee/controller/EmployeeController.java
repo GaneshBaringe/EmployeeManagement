5b4e90fbcf7a0eb844e24fae04ea180c4425d5a2
Shivani-Pangire <pangireshivani309@gmail.com>
2024-10-25 22:56:40 +0530

    Task done

--- EmployeeManagements/src/main/java/com/avsoft/employee/controller/EmployeeController.java
+++ EmployeeManagements/src/main/java/com/avsoft/employee/controller/EmployeeController.java
@@ -3,10 +3,13 @@ package com.avsoft.employee.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.avsoft.employee.entity.Employee;
 import com.avsoft.employee.service.EmployeeService;
+
 
 @RestController
 public class EmployeeController {
@@ -14,6 +17,11 @@ public class EmployeeController {
 	@Autowired
 	EmployeeService employeeService;
 	
+	@PostMapping("/saveEmployee")
+	public String saveEmployee(@RequestBody Employee emp) {
+		employeeService.saveEmp(emp);
+		return "Created";
+	}
 	
 	@GetMapping("/employee/{id}")
 	public Employee viewEmployee(@PathVariable("id") int id) {
