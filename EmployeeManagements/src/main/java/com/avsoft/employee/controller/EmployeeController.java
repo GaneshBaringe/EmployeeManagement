8207681381daa0563cdbe470ce034efc4f1c2a08
Ganesh Baringe <138757020+GaneshBaringe@users.noreply.github.com>
2024-10-26 16:07:35 +0530

    Revert "Revert "Task_DeleteEmp""

--- EmployeeManagements/src/main/java/com/avsoft/employee/controller/EmployeeController.java
+++ EmployeeManagements/src/main/java/com/avsoft/employee/controller/EmployeeController.java
@@ -1,6 +1,7 @@
 package com.avsoft.employee.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -27,5 +28,9 @@ public class EmployeeController {
 	public Employee viewEmployee(@PathVariable("id") int id) {
 		return employeeService.viewEmployee(id);
 	}
-
+	@DeleteMapping("/delete/{id}")
+     public String deleteEmployee(@PathVariable("id")int id) {
+		employeeService.deleteEmp(id);
+		return "Employee Deleted..";
+	}
 }
